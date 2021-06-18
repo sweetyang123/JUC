e42cc7aaf46f5b683e3897c511e328d76b775a1a
sweetyang123 <84315764+sweetyang123@users.noreply.github.com>
2021-06-18 17:29:57 +0800

    interrupt与sleep，synchronized

---
+++ src/com/thread/Interrupt_sleep.java
@@ -0,0 +1,21 @@
+package com.thread;
+
+import java.util.concurrent.TimeUnit;
+
+public class Interrupt_sleep {
+    public static void main(String[] args) throws InterruptedException {
+        Thread t = new Thread(()->{
+            try {
+                Thread.sleep(10000);
+            } catch (InterruptedException e) {
+                System.out.println("Thread is interrupted");
+                //捕捉到InterruptedException时，系统会自动修改标志位，以便于捕捉后面的打断异常
+                System.out.println(Thread.interrupted());//false
+            }
+        });
+        t.start();
+        TimeUnit.SECONDS.sleep(5);
+        //打断（更改状态）
+        t.interrupt();
+    }
+}
