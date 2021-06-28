95cf18733db79779ecef1a25bc269ee5b36f5066
sweetyang123 <84315764+sweetyang123@users.noreply.github.com>
2021-06-28 17:31:39 +0800

    ThreadPoolExecutor源码二

--- src/com/executor/ThreadPoolExecutorDemo02.java
+++ src/com/executor/ThreadPoolExecutorDemo02.java
@@ -1,10 +1,15 @@
 package com.executor;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.*;
 
 public class ThreadPoolExecutorDemo02 {
     public static void main(String[] args) throws InterruptedException {
-
+        System.out.println(getUserList());
+    }
+    static List getUserList() throws InterruptedException {
+        ArrayList userList = new ArrayList();
         ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(1, 1, 0, TimeUnit.MILLISECONDS, new
                 LinkedBlockingDeque<>(), new ThreadFactory() {
             @Override
@@ -22,9 +27,11 @@ public class ThreadPoolExecutorDemo02 {
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
+            userList.add(1);
             System.out.println("exit");
         });
         threadPoolExecutor.shutdown();
         threadPoolExecutor.awaitTermination(1,TimeUnit.MINUTES);
+        return userList;
     }
 }
