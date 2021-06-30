fb2e77bf871b3daa6de6e6ec1200bb8b34bc1bea
sweetyang123 <84315764+sweetyang123@users.noreply.github.com>
2021-06-30 14:09:12 +0800

    多线程与高并发编程一

--- src/com/threadend/Synchronized01.java
+++ src/com/threadend/Synchronized01.java
@@ -1,7 +1,27 @@
 package com.threadend;
 
 /**
- * 重入锁
+ * 可重入锁：被锁方法再调用加锁方法
  */
 public class Synchronized01 {
+    int count=0;
+    synchronized void m(){
+        System.out.println("hello m");
+
+        try {
+            Thread.sleep(2000);
+            count++;
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        m1();
+        System.out.println(count);
+    }
+    synchronized  void m1(){
+        count--;
+        System.out.println("hello m1");
+    }
+    public static void main(String[] args) {
+        new Synchronized01().m();
+    }
 }
--- src/com/threadend/Synchronized02.java
+++ src/com/threadend/Synchronized02.java
@@ -3,6 +3,41 @@ package com.threadend;
 /**
  * 面试：
  * 模拟银行，对写的方法加锁；对读的方法不加锁，可行吗？
+ * 会出现脏读
  */
 public class Synchronized02 {
+      int count;
+
+    public /*synchronized*/ int get() {
+        return count;
+    }
+
+    public synchronized void set(int count) {
+        try {
+            Thread.sleep(200);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        this.count = count;
+    }
+
+
+    public static void main(String[] args) {
+        Synchronized02 syn = new Synchronized02();
+            new Thread(()->{
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                syn.set(100);
+            }).start();
+        System.out.println(syn.get());
+        try {
+            Thread.sleep(200);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println(syn.get());
+    }
 }
